59e8b8401c6381610a490cc2f668f9137dd7c48d
quotalab-tim <hyunchul.yang@gmail.com>
2024-09-07 17:04:23 +0900

    fix: fix script to apply amazon linux2 ami

--- aws/yhc/prod/pulling/ec2/single_instance.tf
+++ aws/yhc/prod/pulling/ec2/single_instance.tf
@@ -1,9 +1,55 @@
 module "single_instance" {
   source = "../../../../modules/ec2"
   name = "pulling_single_instance"
-#   key_name = "pulling_single_instance_key"
   instance_type = "t2.small"
   subnet_id = data.terraform_remote_state.vpc.outputs.public_subnets_id_by_az["ap-northeast-2a"]
+
+  user_data = <<-EOF
+              #!/bin/bash
+              # Update system
+              yum update -y
+
+              # Install NGINX
+              amazon-linux-extras install nginx1 -y
+              systemctl start nginx
+              systemctl enable nginx
+
+              # Install GraalVM Java 21
+              cd /tmp
+              wget https://github.com/graalvm/graalvm-ce-builds/releases/download/vm-21.0.0/graalvm-ce-java21-linux-amd64-21.0.0.tar.gz
+              tar -xzf graalvm-ce-java21-linux-amd64-21.0.0.tar.gz
+              mv graalvm-ce-java21-21.0.0 /usr/lib/graalvm
+              echo 'export PATH=/usr/lib/graalvm/bin:$PATH' >> /etc/profile
+              echo 'export JAVA_HOME=/usr/lib/graalvm' >> /etc/profile
+              source /etc/profile
+
+              # Install CodeDeploy Agent
+              yum install -y ruby wget
+              cd /home/ec2-user
+              wget https://aws-codedeploy-us-east-1.s3.amazonaws.com/latest/install
+              chmod +x ./install
+              ./install auto
+              service codedeploy-agent start
+
+              # Get and install Let's Encrypt certificate
+              amazon-linux-extras install epel -y
+              yum install -y certbot python2-certbot-nginx
+
+              certbot --nginx -d api.pulling.in --non-interactive --agree-tos --email hyunchul.yang@gmail.com
+
+              # Set up auto-renewal
+              echo "0 0,12 * * * root python -c 'import random; import time; time.sleep(random.random() * 3600)' && certbot renew -q" | sudo tee -a /etc/crontab > /dev/null
+
+              # Restart NGINX to apply changes
+              systemctl restart nginx
+              EOF
+
+  ######
+  # eip
+  ######
+
+#   create_eip = true
+#   eip_domain = data.terraform_remote_state.vpc.id
 
   tags = {
     stack   = "prod"
